1a89d812807ad559891d3a8563b7df7dc01e6969
naggets <asamej7734@ncpschools.org>
2024-12-04 08:43:19 -0500

    Create dontworry.js

---
+++ dontworry.js
@@ -0,0 +1,6 @@
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/eruda";
+document.body.appendChild(script);
+script.onload = function () {
+    eruda.init();
+};
